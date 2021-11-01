d7176dbcc3356cb19fd9f464d28fb8d192907d3f
DEVIL-ALPHA <92577442+mhmdmukrim@users.noreply.github.com>
2021-11-01 11:25:29 +0530

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -17,13 +17,13 @@
 */
 
 function successfullMessage(msg) {
-    return "✅ *pinky*:  ```" + msg + "```"
+    return "✅ *sara*:  ```" + msg + "```"
 }
 function errorMessage(msg) {
-    return "🛑 *pinky*:  ```" + msg + "```"
+    return "🛑 *sara*:  ```" + msg + "```"
 }
 function infoMessage(msg) {
-    return "⏺️ *pinky*:  ```" + msg + "```"
+    return "⏺️ *sara*:  ```" + msg + "```"
 }
 
 
